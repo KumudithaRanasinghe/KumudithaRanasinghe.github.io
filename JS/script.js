7e4a5c3cf8edd2f580b3fc6e5f5a8080bf4b271d
Kumuditha Ranasinghe <ranasinghe24125@gmail.com>
2024-12-31 20:11:37 +0530

    fix: add content to the home and about section

--- JS/script.js
+++ JS/script.js
@@ -50,7 +50,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 // typed js
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'E-Sport Player', 'Game Designer'],
+    strings: ['Backend Developer', 'E-Sport Player', 'Game Designer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
