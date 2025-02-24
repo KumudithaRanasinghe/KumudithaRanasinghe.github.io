fb3189746aa077d85b582d4b641af3d6102019b2
Kumuditha Ranasinghe <ranasinghe24125@gmail.com>
2025-02-24 23:30:42 +0530

    fix: change typed text

--- JS/script.js
+++ JS/script.js
@@ -50,7 +50,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 // typed js
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Backend Developer', 'E-Sport Player', 'Game Designer'],
+    strings: ['Web Developer', 'Backend Developer', 'E-Sport Player'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
